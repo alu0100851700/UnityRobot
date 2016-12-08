e043b0378a29df18ff3365bc582c5b6eae7ae134
David Redondo Durand <davidfrd2@gmail.com>
2016-12-08 19:07:56 +0000

    Added prefab rotation joint and prefab lineal joint

---
+++ Assets/Prefab/Joint1/PrefabJoint1.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class PrefabJoint1 : MonoBehaviour {
+	//****** CONSTATS *******
+	const bool X =  true;
+	const bool Z =  false;
+
+	bool rotationSign;
+	bool rotationAxis;
+	public float a;
+	Transform vChildren;
+
+	// Use this for initialization
+	void Start () {
+		rotationAxis = Z;
+		rotationSign = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//SET TEXT ANGLE
+		//GetComponent<TextMesh>().text = "  " + this.transform.FindChild ("visibleObject").localEulerAngles.z.ToString ("F2") + "º";
+		rotateChildrens (a);
+
+	}
+
+	public bool rotateChildrens(float angle){
+		foreach (Transform children in transform) {
+			if (children.name.Contains ("Joint")) {
+				children.RotateAround (
+					transform.position,
+					rotationAxis == Z ? transform.forward :  transform.right,
+					rotationSign? -angle : angle);
+			}
+		}
+
+		return true;
+	}
+
+	public void rotateOverZ(bool sign = true){
+		rotationAxis = Z;
+		rotationSign = sign;
+		if(sign == true)
+			transform.FindChild ("visibleObject").localRotation = Quaternion.Euler (0,90,0);
+		else
+			transform.FindChild ("visibleObject").localRotation = Quaternion.Euler (0,-90,0);
+	}
+
+	public void rotateOverX(bool sign = true){
+		rotationAxis = X;
+		rotationSign = sign;
+		if(sign == true)
+			transform.FindChild ("visibleObject").localRotation = Quaternion.Euler (0,0,0);
+		else
+			transform.FindChild ("visibleObject").localRotation = Quaternion.Euler (0,-180,0);
+	}
+
+	public void linkSize(float size){
+		transform.FindChild ("visibleObject/Link").localScale = new Vector3 (1, size, 1);
+	}
+
+}
---
+++ Assets/Prefab/RobotBase/RobotBase.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RobotBase : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (transform.FindChild ("baseAxis/joint1") != null)
+			transform.FindChild ("visibleObject").localPosition = new Vector3(0, -0.23f, 0);
+		else
+			transform.FindChild ("visibleObject").localPosition = new Vector3(0, -0.105f, 0);
+	}
+}
